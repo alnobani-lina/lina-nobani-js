8e4b42b4a0cc23f1ff946321a3a4fbae48b6026d
alnobani-lina <60358714+alnobani-lina@users.noreply.github.com>
2020-05-19 22:08:28 +0300

    java

--- M01S01/ex02/app.js
+++ M01S01/ex02/app.js
@@ -1,1 +1,16 @@
+var inputlength = document.getElementById('length')
+inputwidth = document.getElementById('width')
+elementresult = document.getElementById('result')
+form = document.querySelector('form');
 
+form.addEventListener('submit' , function(e) {
+    var length = inputlength.value || 0,
+    width = inputwidth.value || 0,
+    result = 0;
+
+    result = length * width;
+
+    elementresult.innerText = result;
+
+    e.preventDefault();
+}, false); 
