783602b5c04535a6fc7ef0ead3a825035ee79672
alnobani-lina <60358714+alnobani-lina@users.noreply.github.com>
2020-06-18 22:04:51 +0300

    curs6

---
+++ M01S06/ex03/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaaded', function(){
+    let button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+    alert('am apasat botton.');
+});
+});
