70896c0d5fa8b9b1ec7725e318eb505d42f256d9
alnobani-lina <60358714+alnobani-lina@users.noreply.github.com>
2020-06-23 22:20:06 +0300

    hangman

--- M01S06/ex04/app.js
+++ M01S06/ex04/app.js
@@ -1,5 +1,27 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+(() => {
+  let showMessage = (message) => {
+    let messagePar = document.createElement('p');
+
+    messagePar.innerText = message;
+    messagePar.classList.add('message');
+
+    document.body.appendChild(messagePar);
+
+    setTimeout(() => {
+      messagePar.remove();
+    }, 200)
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+      let stage = document.querySelector('.stage');
+
+      stage.addEventListener('mouseover', () => {
+        console.log('mouseul este pe scena');
+        showMessage('mouse is');
+      });
+
+      stage.addEventListener('mouseout', () => {
+        console.log('mouse not');
+        showMessage('mouse is not');
+      });
   });
 })();
