ba4ccf22ef4ccec42fffe89698e5624d19294dfe
alnobani-lina <60358714+alnobani-lina@users.noreply.github.com>
2020-07-06 21:32:51 +0300

    homework

--- M02S01/ex03/app.js
+++ M02S01/ex03/app.js
@@ -1,48 +1,73 @@
 let Car = {
-    make: '',
-    color: '',
-    wheels: 0,
-    speed: 0,
-    topSpeed: 140,
-    topReverseSpeed: -70,
-    displaySpeed: function () {
-      console.log(`Viteza curenta este: ${this.speed}`);
-    },
-    accelerate: function () {
-      this.setspeed(++this.speed);
-    },
-    decelerate: function () {
-      this.speed--;
+  make: '',
+  color: '',
+  wheels: 0,
+  speed: 0,
+  topSpeed: 140,
+  topReverseSpeed: -70,
+
+  // add areLightsOn.
+  areLightsOn: false,
+
+  displaySpeed: function () {
+    console.log(`Viteza curenta este: ${this.speed}`);
+  },
+  accelerate: function () {
+    this.setSpeed(++this.speed);
+  },
+  decelerate: function () {
+    this.setSpeed(--this.speed);
+  },
+  setSpeed: function (speed) {
+    if (speed > this.topSpeed) {
+      this.speed = this.topSpeed;
       this.displaySpeed();
-    },
-
-    setspeed: function (speed) {
-        if (speed > this.topSpeed) {
-            this.speed = this.topSpeed;
-            this.displaySpeed();
-            return;
-        }
-
-        if (speed > this.topReverseSpeed) {
-            this.speed = this.topReverseSpeed;
-            this.displaySpeed();
-            return;
-        }
-
-        this.speed = speed;
-        this.displaySpeed();
+      return;
     }
-  };
+
+    if (speed < this.topReverseSpeed) {
+      this.speed = this.topReverseSpeed;
+      this.displaySpeed();
+      return;
+    }
+
+    this.speed = speed;
+    this.displaySpeed();
+  },
+  // add turnLightsOn() and turnLightsOff(). 
+
+  turnLightsOn: function () {
+   this.areLightsOn = true;
+  },
+
+  turnLightsOff: function () {
+    this.areLightsOn = false;
+  },
+
+  // add flashLights.
   
-  let audi = Object.create(Car);
-  audi.make = 'Audi';
-  audi.color = 'black';
-  audi.wheels = 4;
-  audi.speed = 0;
-  audi.topSpeed = 220;
+  flashLights: function () {
+    this.turnLightsOn();
 
-  console.log(audi);
+    window.setTimeout(() => {
+      this.turnLightsOff();
+    }, 3000);
+  }
+};
 
 
-  audi.setspeed
+let audi = Object.create(Car);
+audi.make = 'Audi';
+audi.color = 'black';
+audi.wheels = 4;
+audi.speed = 0;
+audi.topSpeed = 220;
 
+console.log(audi);
+
+// Adauga metoda setSpeed(), proprietatile topSpeed si topReverseSpeed si implementeaza protectiile deja cunoscute.
+// Seteaza topSpeed la 140 apoi ruleaza metoda setSpeed() pentru a face viteza curenta 140.
+// Ruleaza metoda accelerate().
+
+audi.setSpeed(220);
+audi.accelerate();
--- M02S01/ex04/app.js
+++ M02S01/ex04/app.js
@@ -3,15 +3,23 @@ class Car {
       left = 100,
       top = 100,
       color,
-      topColor
+      topColor,
+      wheelColor,
+      capColor,
+      interval
     ) {
       this.left = left;
       this.top = top;
       this.color = color;
       this.topColor = topColor ? topColor : color;
+      this.wheelColor = wheelColor ? wheelColor : color;
+      this.capColor = capColor ? capColor : color;
+      this.interval = interval;
     }
-
+  
     areLightsOn = false;
+    areBreaksOn = false;
+    areHazardsOn = false;
   
     frame = document.createElement('div');
     car = document.createElement('div');
@@ -23,56 +31,114 @@ class Car {
     wheelFront = document.createElement('div');
     wheelCapBack = document.createElement('div');
     wheelCapFront = document.createElement('div');
-
-    turnLightsOn() {
-        this.areLightsOn = true;
-
-        this.lightFront.classList.add('light--on');
-    }
-
-    turnLightsOff () {
-        this.areLightsOn = false;
-
-        this.lightFront.classList.add('light--on');
-    }
-
-    moveFrame (left, top) {
-        if (!left || !top) {
-            return;
-        }
-
-        this.left = left;
-        this.top = top;
-
-        this.frame.style.cssText = `left: ${this.left}px; top: ${this.top}px`;
-    }
-
-    setColor (color) {
-        if (!color) {
-            return;
-        }
-
-        this.color = color;
-
-        this.car.style.backgroundColor = this.color;
-    }
-
-    setTopColor (color) {
-        if (!color) {
-            return;
-        }
-
-        this.Topcolor = color;
-
-        this.carTop.style.backgroundColor = this.topColor;
-    }
-
-
   
+    turnLightsOn () {
+      this.areLightsOn = true;
+  
+      this.lightFront.classList.add('light--on');
+    }
+  
+    turnLightsOff () {
+      this.areLightsOn = false;
+  
+      this.lightFront.classList.remove('light--on');
+    }
+  
+    // Creeaza metode numite engageBreak() si disenagageBreak() care vor aprinde respectiv stinge farul din spate. 
+    engageBreak () {
+      this.areBreaksOn = true;
+  
+      this.lightBack.classList.add('light--on');
+    }
+  
+    disengageBreak () {
+      this.areBreaksOn = false;
+  
+      this.lightBack.classList.remove('light--on');
+    }
+  
+    // Creeaza o metoda numita toggleHazards() care sa functioneze ca avarie si sa aprinda si sa stinga farurile repetat Foloseste o proprietate numita areHazardsOn pentru a putea tine cont de statusul sistemului.
+  
+    toggleHazards () {
+      this.areHazardsOn = !this.areHazardsOn;
+  
+      if (this.areHazardsOn === true ) {
+        this.lightBack.classList.add('light--hz');
+        this.lightFront.classList.add('light--hz');
+      } else {
+        this.lightBack.classList.remove('light--hz');
+        this.lightFront.classList.remove('light--hz');
+      }
+  
+      if (this.interval === null) {
+        this.interval = setInterval (() => {
+          this.toggleHazards();
+        }, 1000);
+      } else {
+        clearInterval (this.interval);
+      }
+  
+      return this.areHazardsOn;
+    }
+  
+    moveFrame (left, top) {
+      if (!left || !top) {
+        return;
+      }
+  
+      this.left = left;
+      this.top = top;
+  
+      this.frame.style.cssText = `left: ${this.left}px; top: ${this.top}px;`;
+    }
+  
+    setColor (color) {
+      if (!color) {
+        return;
+      }
+  
+      this.color = color;
+  
+      this.car.style.backgroundColor = this.color;
+    }
+  
+    setTopColor (color) {
+      if (!color) {
+        return;
+      }
+  
+      this.topColor = color;
+  
+      this.carTop.style.backgroundColor = this.topColor;
+    }
+  
+    // Folosind fisierele rezultate din exercitiul 04 Creeaza configurare pentru culoarea rotilor si a capacelor de roti si metode pentru schimbarea lor dinamica.
+  
+    setWheelColor (color) {
+      if (!color) {
+        return;
+      }
+  
+      this.wheelColor = color;
+  
+      this.wheelBack.style.backgroundColor = this.wheelColor;
+      this.wheelFront.style.backgroundColor = this.wheelColor;
+    }
+  
+    setCapColor (color) {
+      if (!color) {
+        return;
+      }
+  
+      this.capColor = color;
+  
+      this.wheelCapBack.style.backgroundColor = this.capColor;
+      this.wheelCapFront.style.backgroundColor = this.capColor;
+    }
   
     render () {
       this.frame.classList.add('frame');
-      this.frame.style.cssText = `left: ${this.left}px; top: ${this.top}px`;
+      this.frame.style.cssText = `left: ${this.left}px; top: ${this.top}px;`;
   
       // create car div
       this.car.classList.add('car', 'frame__car');
@@ -97,12 +163,16 @@ class Car {
       // create wheels
       this.wheelBack.classList.add('wheel', 'car__wheel', 'car__wheel--back');
       this.wheelFront.classList.add('wheel', 'car__wheel', 'car__wheel--front');
+      this.wheelBack.style.backgroundColor = this.wheelColor;
+      this.wheelFront.style.backgroundColor = this.wheelColor;
       this.carBody.appendChild(this.wheelBack);
       this.carBody.appendChild(this.wheelFront);
   
       // create hubcaps
       this.wheelCapBack.classList.add('wheel__cap');
       this.wheelCapFront.classList.add('wheel__cap');
+      this.wheelCapBack.style.backgroundColor = this.capColor;
+      this.wheelCapFront.style.backgroundColor = this.capColor;
       this.wheelBack.appendChild(this.wheelCapBack);
       this.wheelFront.appendChild(this.wheelCapFront);
   
@@ -110,5 +180,5 @@ class Car {
     }
   }
   
-  let car = new Car(15, 250, 'gray', 'lightblue');
+  let car = new Car(10, 20, 'gray', 'lightblue', 'black', 'silver');
   car.render();
