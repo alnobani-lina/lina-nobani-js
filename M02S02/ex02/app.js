8697251efcf20f1a389cf1b7d5c8ca74430a0dbc
alnobani-lina <60358714+alnobani-lina@users.noreply.github.com>
2020-07-09 22:11:05 +0300

    curs2

--- M02S02/ex02/app.js
+++ M02S02/ex02/app.js
@@ -1,22 +1,91 @@
 $(document).ready(() => {
     let $form = $('#personForm');
-    let $personDetails = $('.personDetails');
-
+    let $personDetails = $('.person-details');
+  
+    let textCaptureForm = () => {
+      let $formFragment = $('<div>');
+  
+      return $formFragment
+        .append('<label for="skill-rename">Editare</label>')
+        .append(
+          $('<input>', {
+            type: 'text',
+            name: 'skill-rename',
+          }),
+        )
+        .append(
+          `
+            <button type="button" id="editSkill">Salvare</button>
+            <button type="button" id="cancelEditSkill">Anulare</button>
+          `,
+        );
+    };
+  
+    $('body').append(textCaptureForm());
+  
     $form.on('submit', (event) => {
-        let formData = $(event.currentTarget).serializeArray();
-        
-        let $personFragment = $('<div>');
-
-        $personDetails.empty();
-        formFields.forEach((field) => {
-            $personFragment.append($('<p>', {
-                text: `${field.name}: ${field.value}`,
-            })
-           );
-        });
-
-        $personFragment.appendTo($personDetails);
-
-        event.preventDefault();
+      let formFields = $(event.currentTarget).serializeArray();
+      let $personFragment = $('<div>');
+  
+      $personDetails.empty();
+  
+      formFields.forEach((field) => {
+        let $labelElement = $(`input[name="${field.name}"]`).prev();
+        let displayName = $labelElement.text();
+  
+        $personFragment.append(
+          $('<p>', {
+            text: `${displayName} ${field.value}`,
+          }),
+        );
+      });
+  
+      $personFragment.appendTo($personDetails);
+      // $personDetails.empty().append($personFragment);
+  
+      event.currentTarget.reset();
+      event.preventDefault();
     });
-});
+  
+    $('#addSkill').on('click', (event) => {
+      // let skillName = $(event.currentTarget).prev().val();
+      let $skillInput = $(event.currentTarget).prev();
+      let skillName = $skillInput.val();
+  
+      if (skillName.length < 1) {
+        return;
+      }
+  
+      let ulClass = 'skills-ul';
+      let $skillsUl = $(`.${ulClass}`);
+  
+      if ($skillsUl.length < 1) {
+        $skillsUl = $('<ul>', {
+          class: ulClass,
+        })
+          .appendTo('.person-skills')
+          .on('click', 'skill-delete', (event) => {
+            // console.log(event.currentTarget);
+            $(event.currentTarget).parent().remove();
+          })
+          .on('click', '.skill-edit', (event) => {
+              $(event.currentTarget).parent().prepend(textCaptureForm());
+          })
+          .on('click', '.skill-edit-cancel', (event) => {
+            $(event.currentTarget).parent().remove();
+          });
+      }
+  
+      $skillsUl.append(
+        `
+          <li>
+            ${skillName}
+            <button type="button", class="skill-delete">Sterge</button>
+            <button class="skill-edit">edit</button>
+          </li>
+        `,
+      );
+  
+      $skillInput.val('');
+    });
+  });
